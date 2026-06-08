2cc53b6f98b25c12c61f6774a957899f3d526519
Akshat-Dev-tech <akiiwork1234@gmail.com>
2026-06-09 00:19:12 +0530

    feat:added otp in state logic logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ function App() {
 
   const [otp , setotp] = useState([])
 
-  const handleChange=(e)=>{
+  const handleChange=(e, index)=>{
     const val = e.target.value
-    console.log('OTP', val)
-    setotp(val)
+    const nxt = [...otp]
+    nxt[index]=val
+    console.log('SET-OTP', val, index)
+    setotp(nxt)
+    console.log('OTP', otp)
   }
 
   return (
     <>
       <h2>OTP Authenticator</h2>
       <div className="alignotp-box">
-        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={handleChange}></input>
-        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={handleChange}></input>
-        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={handleChange}></input>
-        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={handleChange}></input>
+        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={(e)=>{handleChange(e,0)}}></input>
+        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={(e)=>{handleChange(e,1)}}></input>
+        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={(e)=>{handleChange(e,2)}}></input>
+        <input type='text' id="otp1" className='align-input' maxLength={1} onChange={(e)=>{handleChange(e,3)}}></input>
       </div>
     </>
   )
